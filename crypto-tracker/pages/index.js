9672ec1fea798af08ff9bc0509a30d4d81b17096
telfer17 <davietelfer@hotmail.com>
2021-02-18 16:04:48 +0000

    added search functionality & custom logo

--- crypto-tracker/pages/index.js
+++ crypto-tracker/pages/index.js
@@ -1,12 +1,28 @@
+import { useState } from 'react';
 import CoinList from '../components/CoinList';
 import SearchBar from '../components/SearchBar';
+import Layout from '../components/Layout';
 
 export default function Home({ filteredCoins }) {
+  const [search, setSearch] = useState('');
+
+  const allCoins = filteredCoins.filter(coin =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  const handleChange = e => {
+    e.preventDefault();
+
+    setSearch(e.target.value.toLowerCase());
+  };
+
   return (
-    <div>
-      <SearchBar type="text" placeholder="Search" />
-      <CoinList filteredCoins={filteredCoins} />
-    </div>
+    <Layout>
+      <div className="coin_app">
+        <SearchBar type="text" placeholder="Search" onChange={handleChange} />
+        <CoinList filteredCoins={allCoins} />
+      </div>
+    </Layout>
   );
 }
 
